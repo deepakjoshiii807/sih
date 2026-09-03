a2522b2c55696176e24b40faf2ade1e8077bda7c
Deepak J0shi <178358463+deepakjoshiii807@users.noreply.github.com>
2026-09-03 17:33:35 +0000

    Before: ok now the navbar fix it the labels and all

--- isolate/assets/radix-ui-BxNAREy0.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./framer-motion-BS74gQnS.js";import{r as o}from"./react-vendor-D0hkx8ag.js";var t=o();const m=r(t);export{m as R};
---
+++ isolate/assets/radix-ui-CpvI_6de.js
@@ -0,0 +1,1 @@
+import{g as r}from"./framer-motion-BS74gQnS.js";import{r as o}from"./react-vendor-D0sfmpHS.js";var t=o();const m=r(t);export{m as R};
