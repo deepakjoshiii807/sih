0361c79fecb6d987af7443b45a65518eb108a3d3
Deepak J0shi <178358463+deepakjoshiii807@users.noreply.github.com>
2026-09-05 06:16:48 +0000

    Freebuff: Change /dashboard name to /student

--- src/main.tsx
+++ src/main.tsx
@@ -34,7 +34,7 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
         <Route path="/" element={<AuthPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<StudentDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
         <Route path="/faculty" element={<FacultyDashboard />} />
         <Route path="/industry" element={<IndustryDashboard />} />
 
